f4c1fcfcc9268d095de7d573a0a6507f88c917da
Joonyoung Hong <hjunyoung10@gmail.com>
2023-12-03 16:24:53 +0900

    Feat: CartRepository에 deleteByUserAndProductIn 메서드 추가 [#4]

--- src/main/java/com/example/purebasketbe/domain/cart/CartRepository.java
+++ src/main/java/com/example/purebasketbe/domain/cart/CartRepository.java
@@ -1,10 +1,18 @@
 package com.example.purebasketbe.domain.cart;
 
 import com.example.purebasketbe.domain.cart.entity.Cart;
+import com.example.purebasketbe.domain.product.entity.Product;
+import com.example.purebasketbe.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository <Cart,Long> {
 
+    @Modifying
+    @Query("DELETE FROM Cart c WHERE c.user=:user AND c.product IN (:products)")
+    void deleteByUserAndProductIn(User user, List<Product> products);
 }
