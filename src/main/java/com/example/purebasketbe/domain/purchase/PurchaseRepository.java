88d30633fbd4c1b8ecf6e604c3b4814c0dfd5d10
Joonyoung Hong <hjunyoung10@gmail.com>
2023-12-03 16:23:12 +0900

    Feat: PurchaseRepository에 findAllByUser 메서드 추가 [#4]

--- src/main/java/com/example/purebasketbe/domain/purchase/PurchaseRepository.java
+++ src/main/java/com/example/purebasketbe/domain/purchase/PurchaseRepository.java
@@ -1,10 +1,15 @@
 package com.example.purebasketbe.domain.purchase;
 
 import com.example.purebasketbe.domain.purchase.entity.Purchase;
+import com.example.purebasketbe.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
+    @Query("SELECT p FROM Purchase p JOIN FETCH p.product WHERE p.user = :user")
+    List<Purchase> findAllByUser(User user);
 }
