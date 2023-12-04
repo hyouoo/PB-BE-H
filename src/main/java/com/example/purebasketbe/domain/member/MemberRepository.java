72ab4bf5de2f1354c5599c78017329caa5b3e1b3
Seoyoon <wideskyinme@gmail.com>
2023-12-04 16:46:02 +0900

    Feat: 로그인 회원가입 기능 구현

--- src/main/java/com/example/purebasketbe/domain/member/MemberRepository.java
+++ src/main/java/com/example/purebasketbe/domain/member/MemberRepository.java
@@ -4,7 +4,11 @@ import com.example.purebasketbe.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 }
