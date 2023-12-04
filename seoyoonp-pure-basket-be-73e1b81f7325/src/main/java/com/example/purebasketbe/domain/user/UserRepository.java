7a16f1220afde7932d3fa0e221e5b754a22e6e01
liber9194 <kim960928@naver.com>
2023-12-05 00:58:59 +0900

    Feat : cart crud 1차 구현

--- seoyoonp-pure-basket-be-73e1b81f7325/src/main/java/com/example/purebasketbe/domain/user/UserRepository.java
+++ seoyoonp-pure-basket-be-73e1b81f7325/src/main/java/com/example/purebasketbe/domain/user/UserRepository.java
@@ -1,10 +1,10 @@
 package com.example.purebasketbe.domain.user;
 
-import com.example.purebasketbe.domain.user.entity.User;
+import com.example.purebasketbe.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Member, Long> {
 
 }
