29e9c68f7eb7e184275ab0e276376be59dbdfcdc
Sanghyu Lee <hyouoo@gmail.com>
2023-12-07 22:29:31 +0900

    Feat: BackOfficeProductController 구현 [#5]

--- src/main/java/com/example/purebasketbe/domain/product/BackOfficeProductController.java
+++ src/main/java/com/example/purebasketbe/domain/product/BackOfficeProductController.java
@@ -1,5 +1,6 @@
 package com.example.purebasketbe.domain.product;
 
+import com.example.purebasketbe.domain.product.dto.ProductListResponseDto;
 import com.example.purebasketbe.domain.product.dto.ProductRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,28 +15,47 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/backoffice")
-public class BackOfficeController {
+@RequestMapping("/api/backoffice/products")
+public class BackOfficeProductController {
 
     private final ProductService productService;
+    private final String adminLandingPath = "/api/backoffice/products";
+
+    @GetMapping
+    public ResponseEntity<ProductListResponseDto> getProducts(
+            @RequestParam(defaultValue = "1", required = false) int eventPage,
+            @RequestParam(defaultValue = "1", required = false) int page) {
+        ProductListResponseDto responseBody = productService.getProducts(eventPage - 1, page - 1);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<ProductListResponseDto> searchProducts(
+            @RequestParam String query,
+            @RequestParam(defaultValue = "", required = false) String category,
+            @RequestParam(defaultValue = "1", required = false) int eventPage,
+            @RequestParam(defaultValue = "1", required = false) int page) {
+        ProductListResponseDto responseBody = productService.searchProducts(query, category, eventPage - 1, page - 1);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void> registerProduct(@RequestBody @Validated ProductRequestDto requestDto,
+    public ResponseEntity<Void> registerProduct(@RequestPart(value = "dto") @Validated ProductRequestDto requestDto,
                                                 @RequestPart(value = "files") List<MultipartFile> files) {
         productService.registerProduct(requestDto, files);
         return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                .header("Location", "/api/backoffice")
+                .header("Location", adminLandingPath)
                 .build();
     }
 
     @PutMapping(value = "/{productId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> updateProduct(@PathVariable Long productId,
-                                              @RequestBody @Validated ProductRequestDto requestDto,
+                                              @RequestPart(value = "dto") @Validated ProductRequestDto requestDto,
                                               @RequestPart(value = "files", required = false) List<MultipartFile> files) {
         List<MultipartFile> fileList = Optional.ofNullable(files).orElse(List.of());
         productService.updateProduct(productId, requestDto, fileList);
         return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                .header("Location", "/api/backoffice")
+                .header("Location", adminLandingPath)
                 .build();
     }
 
@@ -43,7 +63,7 @@ public class BackOfficeController {
     public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
         productService.deleteProduct(productId);
         return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                .header("Location", "/api/backoffice")
+                .header("Location", adminLandingPath)
                 .build();
     }
 }
