91496140ac62bdc82c50ad68352ff98b6d8eaa13
Sanghyu Lee <hyouoo@gmail.com>
2023-12-08 11:12:44 +0900

    Feat: Cart domain 수정 [#3]

--- src/main/java/com/example/purebasketbe/domain/cart/CartService.java
+++ src/main/java/com/example/purebasketbe/domain/cart/CartService.java
@@ -4,7 +4,9 @@ import com.example.purebasketbe.domain.cart.dto.CartRequestDto;
 import com.example.purebasketbe.domain.cart.dto.CartResponseDto;
 import com.example.purebasketbe.domain.cart.entity.Cart;
 import com.example.purebasketbe.domain.member.entity.Member;
+import com.example.purebasketbe.domain.product.ImageRepository;
 import com.example.purebasketbe.domain.product.ProductRepository;
+import com.example.purebasketbe.domain.product.entity.Image;
 import com.example.purebasketbe.domain.product.entity.Product;
 import com.example.purebasketbe.domain.recipe.RecipeRepository;
 import com.example.purebasketbe.domain.recipe.entity.Recipe;
@@ -20,48 +22,40 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CartService {
+
     private final CartRepository cartRepository;
     private final ProductRepository productRepository;
     private final RecipeRepository recipeRepository;
+    private final ImageRepository imageRepository;
 
     @Transactional
     public void addToCart(Long productId, CartRequestDto requestDto, Member member) {
-        Product product = productRepository.findById(productId).orElseThrow(
-                () -> new NullPointerException("해당 상품이 존재하지 않습니다.")
-        );
-
-        Cart cart = cartRepository.save(Cart.of(requestDto, member, product));
+        Product product = findAndValidateProduct(productId);
+        Cart newCart = Cart.of(product, member, requestDto);
+        cartRepository.save(newCart);
     }
 
     @Transactional(readOnly = true)
-    public List<CartResponseDto> getCart(Member member) {
-
-        List<Cart> cartList = cartRepository.findAllByMember(member);
-
-        return cartList.stream().map(cart -> {
-            Product product = getProductById(cart.getProduct().getId());
-            return CartResponseDto.of(product, cart);
-        }).toList();
+    public List<CartResponseDto> getCartList(Member member) {
+        return cartRepository.findAllByMember(member).stream()
+                .map(cart -> {
+                    Product product = findAndValidateProduct(cart.getProduct().getId());
+                    Image image = findImage(product.getId());
+                    return CartResponseDto.of(product, image, cart);
+                }).toList();
     }
 
     @Transactional
-    public void changeCart(Long productId, CartRequestDto requestDto, Member member) {
-        Cart cart = cartRepository.findByProductId(productId).orElseThrow(
-                () -> new NullPointerException("해당하는 제품이 존재하지 않습니다.")
-        );
-
-        cart.updateCart(requestDto);
+    public void updateCart(Long productId, CartRequestDto requestDto, Member member) {
+        Cart cart = findAndValidateCart(productId, member);
+        cart.changeAmount(requestDto);
     }
 
     @Transactional
-    public void deleteCart(Long productId) {
-        Cart cart = cartRepository.findByProductId(productId).orElseThrow(
-                () -> new NullPointerException("해당하는 제품이 존재하지 않습니다.")
-        );
-
+    public void deleteCart(Long productId, Member member) {
+        Cart cart = findAndValidateCart(productId, member);
         cartRepository.delete(cart);
     }
-
 
     @Transactional
     public void addRecipeRelatedProductsToCarts(Long recipeId, Member member) {
@@ -71,14 +65,28 @@ public class CartService {
 
         List<Product> productList = recipe.getRecipeProductList().stream().map(RecipeProduct::getProduct).toList();
         cartRepository.deleteAllByMemberAndProductIn(member, productList);
-        List<Cart> cartList = productList.stream().map(product -> Cart.of(product, member)).toList();
+        List<Cart> cartList = productList.stream().map(product -> Cart.of(product, member, null)).toList();
         cartRepository.saveAll(cartList);
     }
 
-    private Product getProductById(Long id) {
-        return cartRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("해당하는 상품이 없습니다.")
-        ).getProduct();
+    private Product findAndValidateProduct(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND)
+        );
+        if (product.getStock() == 0 || product.isDeleted()) {
+            throw new CustomException(ErrorCode.NOT_ENOUGH_PRODUCT);
+        }
+        return product;
     }
 
+    private Cart findAndValidateCart(Long productId, Member member) {
+        return cartRepository.findByProductIdAndMember(productId, member).orElseThrow(
+                () -> new CustomException(ErrorCode.INVALID_CART_ITEM)
+        );
+    }
+
+    private Image findImage(Long productId) {
+        return imageRepository.findAllByProductId(productId).stream().findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_IMAGE));
+    }
 }
