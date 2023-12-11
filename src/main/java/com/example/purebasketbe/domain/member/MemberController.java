beb64a8a7120b0d8310588b8d9a37df9fb431551
Seoyoon <wideskyinme@gmail.com>
2023-12-11 16:23:52 +0900

    !HOTFIX: validated 어노테이션 추가

--- src/main/java/com/example/purebasketbe/domain/member/MemberController.java
+++ src/main/java/com/example/purebasketbe/domain/member/MemberController.java
@@ -4,6 +4,7 @@ import com.example.purebasketbe.domain.member.dto.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> registerMember(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<Void> registerMember(@RequestBody @Validated SignupRequestDto requestDto) {
         memberService.registerMember(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
