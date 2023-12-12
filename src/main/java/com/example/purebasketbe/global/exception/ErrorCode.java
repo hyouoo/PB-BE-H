e033caa56699c59ed80023dfb647ecd2f5a8aad0
Seoyoon <wideskyinme@gmail.com>
2023-12-12 15:16:48 +0900

    Update: Admin login 개발에 따른 인증 인가 로직 수정

--- src/main/java/com/example/purebasketbe/global/exception/ErrorCode.java
+++ src/main/java/com/example/purebasketbe/global/exception/ErrorCode.java
@@ -49,6 +49,7 @@ public enum ErrorCode {
     ACCESS_DENIED(HttpStatus.FORBIDDEN.value(), "접근 권한이 없습니다."),
     INSUFFICIENT_POINT(HttpStatus.BAD_REQUEST.value(), "포인트가 부족합니다."),
     ORDER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "처리할 주문이 없습니다."),
+    UNSUPPORTED_USER_TYPE(HttpStatus.BAD_REQUEST.value(), "잘못된 사용자 유형입니다."),
     UNEXPECTED_ERROR(443, "예상치 못한 오류가 발생했습니다."),;
 
     private final int httpStatus;
