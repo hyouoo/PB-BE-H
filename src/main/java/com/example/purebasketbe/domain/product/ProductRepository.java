200220797661aa8a915faee3c0188778b0b2b811
Joonyoung Hong <hjunyoung10@gmail.com>
2023-12-25 22:18:17 +0900

    Fix: Redis cache 적용 시 SerializationException 해결

--- src/main/java/com/example/purebasketbe/domain/product/ProductRepository.java
+++ src/main/java/com/example/purebasketbe/domain/product/ProductRepository.java
@@ -4,11 +4,11 @@ import com.example.purebasketbe.domain.product.entity.Event;
 import com.example.purebasketbe.domain.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
