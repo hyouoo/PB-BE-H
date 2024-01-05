cd64394fcd8cc1473f3b8c5444897218caa26943
Joonyoung Hong <hjunyoung@gmail.com>
2024-01-05 18:14:31 +0900

    Update: CORS 설정 변경

--- src/main/java/com/example/purebasketbe/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/purebasketbe/global/config/WebSecurityConfig.java
@@ -96,7 +96,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://pure-basket.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
