0c719b9531c7c1eea33ad985af15e298c6ef50f3
Sanghyu Lee <hyouoo@gmail.com>
2024-01-08 23:53:37 +0900

    Refactor: Product-Stock 단방향 변경

--- src/main/java/com/example/purebasketbe/domain/product/StockRepository.java
+++ src/main/java/com/example/purebasketbe/domain/product/StockRepository.java
@@ -1,15 +1,10 @@
 package com.example.purebasketbe.domain.product;
 
 import com.example.purebasketbe.domain.product.entity.Stock;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
+    Stock findByProductId(Long productId);
 }
