4a73e9c39e87c4e1cf9d6d054c1a4aea33d13d9b
Sanghyu Lee <hyouoo@gmail.com>
2024-01-17 22:04:14 +0900

    Update: Local setting

--- src/main/java/com/example/purebasketbe/global/config/RedisConfig.java
+++ src/main/java/com/example/purebasketbe/global/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -22,31 +23,37 @@ import java.time.Duration;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
-    @Value("${spring.data.redis.sentinel.master}")
-    private String sentinel1;
-    @Value("${spring.data.redis.sentinel.replica1}")
-    private String sentinel2;
-    @Value("${spring.data.redis.sentinel.replica2}")
-    private String sentinel3;
-    @Value("${spring.data.redis.sentinel.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password}")
-    private String password;
-
+//    @Value("${spring.data.redis.sentinel.master}")
+//    private String sentinel1;
+//    @Value("${spring.data.redis.sentinel.replica1}")
+//    private String sentinel2;
+//    @Value("${spring.data.redis.sentinel.replica2}")
+//    private String sentinel3;
+//    @Value("${spring.data.redis.sentinel.port}")
+//    private int port;
+//
+//    @Value("${spring.data.redis.password}")
+//    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisSentinelConfiguration sentinelConfig  = new RedisSentinelConfiguration()
-                .master("mymaster")
-                .sentinel(sentinel1, port)
-                .sentinel(sentinel2, port)
-                .sentinel(sentinel3, port);
-
-        sentinelConfig.setPassword(password);
-        return new LettuceConnectionFactory(sentinelConfig );
+        RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration("localhost",6379);
+        return new LettuceConnectionFactory(standaloneConfiguration);
 
     }
+
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisSentinelConfiguration sentinelConfig  = new RedisSentinelConfiguration()
+//                .master("mymaster")
+//                .sentinel(sentinel1, port)
+//                .sentinel(sentinel2, port)
+//                .sentinel(sentinel3, port);
+//
+//        sentinelConfig.setPassword(password);
+//        return new LettuceConnectionFactory(sentinelConfig );
+//
+//    }
 
     // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
     @Bean
--- src/main/java/com/example/purebasketbe/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/purebasketbe/global/exception/CustomExceptionHandler.java
@@ -2,17 +2,18 @@ package com.example.purebasketbe.global.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class    ExceptionHandler {
+public class CustomExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {CustomException.class})
+    @ExceptionHandler(value = {CustomException.class})
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult()
                 .getAllErrors()
