b378eed696c7632f0939d30781b9a8b9507a8066
Sanghyu Lee <hyouoo@gmail.com>
2024-01-17 22:03:02 +0900

    Update: Slack 알람 수정

--- src/main/java/com/example/purebasketbe/global/slack/SlackAppender.java
+++ src/main/java/com/example/purebasketbe/global/slack/SlackAppender.java
@@ -2,28 +2,36 @@ package com.example.purebasketbe.global.slack;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
+@Configuration
 public class SlackAppender extends AppenderBase<ILoggingEvent> {
 
+    @Value("${slack.webhook.url}")
+    private String webhookUrl;
+
     @Override
-    protected void append(ILoggingEvent eventObject) {
+    protected void append(final ILoggingEvent eventObject) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://hooks.slack.com/services/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";
         Map<String, Object> body = createSlackErrorBody(eventObject);
-        restTemplate.postForEntity(url, body, String.class);
+        log.info("url : {}", webhookUrl);
+        restTemplate.postForEntity(webhookUrl, body, String.class);
     }
 
-    private Map<String, Object> createSlackErrorBody(ILoggingEvent eventObject) {
+    private Map<String, Object> createSlackErrorBody(final ILoggingEvent eventObject) {
         String message = createMessage(eventObject);
         return Map.of(
                 "attachments", List.of(
                         Map.of(
-                                "fallback", "요청을 실패했어요 :cry:",
+                                "fallback", "에러가 발생했어요 :cry:",
                                 "color", "#2eb886",
                                 "pretext", "에러가 발생했어요 확인해주세요 :cry:",
                                 "author_name", "purebasket-be",
@@ -34,9 +42,9 @@ public class SlackAppender extends AppenderBase<ILoggingEvent> {
         );
     }
 
-    private String createMessage(ILoggingEvent eventObject) {
+    private String createMessage(final ILoggingEvent eventObject) {
         String baseMessage = "에러가 발생했습니다.\n";
-        String pattern = baseMessage + "```%s %s %s [%s] - %s```";
+        String pattern = baseMessage + "```%s [%s] [%s] [%s] - %s```";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         return String.format(pattern,
                 simpleDateFormat.format(eventObject.getTimeStamp()),
